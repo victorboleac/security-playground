f70a0d5808bcaca59b0f83fa00ab77815dc8a100
Victor Boleac <victor.boleac@icloud.com>
2024-11-29 10:40:38 +0000

    initial commit

---
+++ src/main/java/com/northcoders/security_playground/SecurityPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.security_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPlaygroundApplication.class, args);
+	}
+
+}
