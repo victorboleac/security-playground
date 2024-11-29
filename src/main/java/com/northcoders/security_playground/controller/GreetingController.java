2e5585bcb98bdd9509c907f4b0148dba802536a0
Victor Boleac <victor.boleac@icloud.com>
2024-11-29 12:33:13 +0000

    retrieving oauth2user details

--- src/main/java/com/northcoders/security_playground/controller/GreetingController.java
+++ src/main/java/com/northcoders/security_playground/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.northcoders.security_playground.controller;
-
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ public class GreetingController {
     }
     @GetMapping("/protected/greeting")
     public String protectedGreeting() {
-        return "Hi, nice to meet you!";
+        OAuth2User userDetails = (OAuth2User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        return "Hi, "+ userDetails.getAttribute("name")+" nice to meet you!";
     }
 }
